90a05ee0116ac8e99deacff594071e0b75adb375
TrentekaHarris <trentekaharris@gmail.com>
2022-12-12 13:15:12 -0500

    added tasks 7 and 8

--- js/index.js
+++ js/index.js
@@ -1,25 +1,16 @@
 
 class Task extends TaskManager
-
 {constructor(tasks)
   {super(tasks)}
-  
 }
-
 const taskNew = new Task();
-
-
 //Variable for form
 const data = document.querySelector('#newTaskForm');
-
-
 function validFormFieldInput(data) {
   const formData = new FormData(data)
-
   //name for fields
   const newTaskNameInput = document.querySelector('#newTaskNameInput');
-
-  const name = newTaskNameInput.value;
+  const name = newTaskNameInput.value
   const newTaskDescription = document.querySelector('#newTaskDescription');
   const description = newTaskDescription.value;
   const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
@@ -27,62 +18,59 @@ function validFormFieldInput(data) {
   const newTaskDueDate = document.querySelector('#newTaskDueDate');
   const dueDate = newTaskDueDate.value;
 
-
  //verify inputs
  console.log("name:  "+name, " description: "+ description, " assignedTo: " + assignedTo, " due date: "+ dueDate, " status: "+status);
-
 //Get form data
   for (const task of formData.entries()) {
     taskNew.addTask(name, description, assignedTo, dueDate);
   }
-
 }
-
 //Hide alert
 document.getElementById("alert").style.display="none";
-
 //function to check values from form
 function hasValue(input) {
-   
    if (input.value.trim() === "") {
     return document.getElementById("alert").style.display="block";
   } else {
     document.getElementById("alert").style.display="none"
-  } 
-
-  
-
+  }
 }
-
 ;
-
 //Event for submit
 document.getElementById("newTaskForm").addEventListener("submit", function (event) {
-  
-  
   //Handle invalid data & prevent default
   event.preventDefault();
-
-  
   //form validation is successful
   let nameValid = hasValue(data.elements['newTaskNameInput']);
   validFormFieldInput(event.target);
   const name = newTaskNameInput.value;
-  const description = newTaskDescription.value; 
-  const assignedTo = newTaskAssignedTo.value; 
-  const dueDate = newTaskDueDate.value; 
-  const status = 'To Do';
+  const description = newTaskDescription.value;
+  const assignedTo = newTaskAssignedTo.value;
+  const dueDate = newTaskDueDate.value;
+  let status = 'Status';
+
   taskHtml = createTaskHtml(name, description, assignedTo, dueDate, status)
-console.log(taskHtml);
+
 taskNew.render()
-
   //Reset form
-
 data.reset();
-
+});
+//call new tasks (see new task in console)
+console.log(taskNew.tasks)
+//adding onclick event
+const tasksList = document.querySelector('#tasksList');
+tasksList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('done-button')) {
+     const parentTask = event.target.parentElement.parentElement;
+       const taskId = Number(parentTask.dataset.taskId);
+        const task = taskNew.getTaskById(tasksList);
+          taskNew.status = 'DONE';
+            taskNew.save();
+            taskNew.render();
+    }
 });
 
-//call new tasks (see new task in console)
-
-console.log(taskNew.tasks)
-
+//delete task event listener
+/*document.getElementById("newTaskForm").addEventListener("click", function(event) {
+  TaskManager.deleteTask(taskId);
+});*/
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,49 +1,32 @@
-
-
 //Using Javascript to Create the Task HTML
-
-const createTaskHtml = (name, description, assignedTo, dueDate, status) =>  
-    `<div class="card-body m-5 border border-danger card-body m-5 row row-cols-4">
+const createTaskHtml = (name, description, assignedTo, dueDate, status, id) =>
+    `<div class="card-body m-5 border border-danger card-body m-5 row row-cols-4" data-task-id=${id}>
     <h5 class="card-title fs-3 col-8">${name}</h5>
     <a href="#" class="btn btn-success col-4">${status}</a>
     <p class="task-description col-12">${description}</p>
-    
     <p class="ask-assignee col-12">${assignedTo}</p>
-    <p class="start-date col-12">Start Date: September 25, 2022</p> 
-    <p class="due-date col-8">${dueDate}</p> 
+    <p class="start-date col-12">Start Date: September 25, 2022</p>
+    <p class="due-date col-8">${dueDate}</p>
     <a href="#" class="btn btn-success col-4">Delete</a>
+    <button class="done-button btn btn-danger col-4">Mark As Done</button>
   </div>`
-   
-  
- 
- 
- 
-  
- 
- 
-
 //Task Manager Class
 class TaskManager {
-   
-   constructor(task = [], currentId) {
+    constructor(task = [], currentId) {
         this._tasks = task;
         this._currentId = currentId;
-            
-   }
+    }
     //Add task to list
     get tasks() {
         return this._tasks;
     }
-
     /*increaseTask(newTask){
         return this._tasks.push(newTask);
     }
-    
     //Increase Id
     currentId () {
         console.log(`TO DD: ${this.tasks.length}`)
     }*/
-
     //Add Task Method
     addTask(newTaskNameInput, newTaskDescription, newTaskAssignedTo, newTaskDueDate, status) {
         const task = {
@@ -54,22 +37,69 @@ class TaskManager {
             newTaskDueDate: newTaskDueDate,
             status: status
         };
-
-             this.tasks.push(task) 
+        this.tasks.push(task)
     }
-   //render method 
-    render() { 
-        const tasksHtmlList = []; 
-        for(let i = 0; i < this.tasks.length; i++){ 
+    //render method
+    render() {
+        const tasksHtmlList = [];
+        for (let i = 0; i < this.tasks.length; i++) {
             const task = this.tasks[i];
-        const date = new Date('October 30, 2022'); 
-        const formattedDate = 'October 30, 2022'; 
-        const taskHtml = createTaskHtml('Finish Organizing Garage', 'Decorations are overtaking the garage. It needs to be organized before the start of the holiday season.', 'Task Assignee: Thomas', 'Due Date: October 31, 2022', 'incomplete'); 
-        tasksHtmlList.push(taskHtml) 
+            const date = new Date('October 30, 2022');
+            const formattedDate = 'October 30, 2022';
+            const taskHtml = createTaskHtml('Finish Organizing Garage', 'Decorations are overtaking the garage. It needs to be organized before the start of the holiday season.', 'Task Assignee: Thomas', 'Due Date: October 31, 2022', 'incomplete');
+            tasksHtmlList.push(taskHtml)
         }
-        const taskList = document.querySelector("#tasksList"); 
+        const taskList = document.querySelector("#tasksList");
         taskList.innerHTML = taskHtml;
-    } 
+    }
+
+//getTaskById to the TaskManager class
+getTaskById(taskID) {
+    let foundTask = "";
+    for (let i = 0; i < this.tasks.length; i++) {
+        let task = this.tasks[i];
+        if (task.id === taskID) {
+            let foundTask = task;
+        }
+    }
+    return foundTask;
 
 }
 
+//task 8  (save & load method to TaskManager)
+save() {
+    const tasksJson = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', tasksJson);
+    const currentId = this.currentId.toString();
+    localStorage.setItem('currentId', currentId);
+}
+load() {
+    if (localStorage.getItem('tasks')) {
+        const tasksJson = localStorage.getItem('tasks');
+        this.tasks = JSON.parse(tasksJson);
+    }
+    if (localStorage.getItem('currentId')) {
+        const currentId = localStorage.getItem('currentId');
+        this.currentId = Number(currentId);
+    }
+}
+
+}
+// Create the deleteTask method
+/*deleteTask(taskId) {
+    // Create an empty array and store it in a new variable, newTasks
+    const newTasks = [];
+
+    // Loop over the tasks
+    for (let i = 0; i < this.tasks.length; i++) {
+        // Get the current task in the loop
+        const task = this.tasks[i];
+
+        // Check if the task id is not the task id passed in as a parameter
+        if (task.id !== taskId) {
+            // Push the task to the newTasks array
+            newTasks.push(task);
+        }
+        return this.tasks = newTasks;
+    }
+}*/
